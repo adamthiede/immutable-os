1cd3a671dca6190bb0ff74b8f7c250ea87c9ddda
adamthiede <me@adamthiede.com>
2024-08-03 11:44:23 -0500

    use fedora base image instead

--- build.sh
+++ build.sh
@@ -12,7 +12,13 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# remove some base packages
+# rpmfusion install/uninstall
+rpm-ostree install \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm"
+
+rpm-ostree update \
+	--uninstall rpmfusion-free-release \
+	--install rpmfusion-free-release
 
 rpm-ostree override remove sddm-wayland-sway sddm \
 	firefox firefox-langpacks sway-config-fedora \
@@ -29,7 +35,7 @@ rpm-ostree install mpv ffmpeg sway foot bemenu j4-dmenu-desktop \
 	doas fastfetch flashrom i3blocks i3status i3 imv isync \
 	jq kanshi keepassxc minetest mosh mousepad mpc mpd ncmpcpp \
 	ncdu nethack newsboat nmap pmbootstrap rsync seahorse \
-	NetworkManager-tui syncthing \
+	NetworkManager-tui syncthing tailscale \
 	xorg-x11-server-Xorg xorg-x11-drv-intel xorg-x11-drv-libinput
 
 #### Example for enabling a System Unit File
