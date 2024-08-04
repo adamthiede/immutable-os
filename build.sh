b3378971132ad600e6c4ac41cf6bfe0865d355ff
adamthiede <me@adamthiede.com>
2024-08-04 18:06:55 -0500

    Revert "use fedora base image instead"
    This reverts commit 1cd3a671dca6190bb0ff74b8f7c250ea87c9ddda.

--- build.sh
+++ build.sh
@@ -12,13 +12,7 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# rpmfusion install/uninstall
-rpm-ostree install \
-  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm"
-
-rpm-ostree update \
-	--uninstall rpmfusion-free-release \
-	--install rpmfusion-free-release
+# remove some base packages
 
 rpm-ostree override remove sddm-wayland-sway sddm \
 	firefox firefox-langpacks sway-config-fedora \
@@ -35,7 +29,7 @@ rpm-ostree install mpv ffmpeg sway foot bemenu j4-dmenu-desktop \
 	doas fastfetch flashrom i3blocks i3status i3 imv isync \
 	jq kanshi keepassxc minetest mosh mousepad mpc mpd ncmpcpp \
 	ncdu nethack newsboat nmap pmbootstrap rsync seahorse \
-	NetworkManager-tui syncthing tailscale \
+	NetworkManager-tui syncthing \
 	xorg-x11-server-Xorg xorg-x11-drv-intel xorg-x11-drv-libinput
 
 #### Example for enabling a System Unit File
